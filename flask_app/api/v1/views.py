59fbc938a4a90e4022ccd96decb4a828c6344499
ANTH0RNY <101851338+ANTH0RNY@users.noreply.github.com>
2023-11-26 18:49:44 +0300

    Done with lights off

--- flask_app/api/v1/views.py
+++ flask_app/api/v1/views.py
@@ -1,7 +1,11 @@
 from . import v1
 from ..models import User
-from flask import jsonify
+from flask import jsonify, request
 from flask_jwt_extended import jwt_required, current_user
+from ..influx import client, bucket, measurement, org
+from influxdb_client import Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+# from app import app
 
 
 @v1.route("/")
@@ -13,3 +17,18 @@ def index():
 @jwt_required()
 def user():
     return jsonify({"id": current_user.id, "username": current_user.username})
+
+
+@v1.route("/lights", methods=["POST"])
+def lights():
+    state = request.json.get("light", None)
+    if state is not None:
+        # print(state)
+        write_client = client.write_api(write_options=SYNCHRONOUS)
+
+        point = Point(measurement).tag("lights", "state").field("light", state)
+        write_client.write(bucket=bucket, org=org, record=point)
+        return jsonify({"light": state})
+    return jsonify({'error': 'Invalid Options'}), 400
+
+    # if (state):
